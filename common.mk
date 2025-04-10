719a852dea0ce1bc0a0cc78df88d9164785df5b8
fiirefly24 <ammadali.fikri@gmail.com>
2025-04-10 13:08:16 +0000

    sm8450-common: Fix path for boot-image-profile.txt
    Ref: LineageOS/android_frameworks_base@c34748c

--- common.mk
+++ common.mk
@@ -472,7 +472,7 @@ PRODUCT_COPY_FILES += \
 
 # Improve performance
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
