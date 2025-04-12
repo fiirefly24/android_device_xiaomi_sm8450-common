f5cfad2e8ed4680e3f221a84d54141252af8c051
fiirefly24 <ammadali.fikri@gmail.com>
2025-04-13 01:14:04 +0200

    Revert "sm8450-common: Add support for ViPER4Android FX Redesign and build it"
    This reverts commit 910f57d8b625c9edea2fd0291659cf8cdea2c8ef.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -9,8 +9,6 @@ include vendor/xiaomi/sm8450-common/BoardConfigVendor.mk
 
 COMMON_PATH := device/xiaomi/sm8450-common
 
-BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
-
 # A/B
 AB_OTA_PARTITIONS += \
     boot \
